eb36b0076c68817151373adc36369bd650b462a2
viktohblake <damanygt@gmail.com>
2026-05-07 18:58:26 +0100

    connect routes to server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,11 +2,16 @@ import express from "express";
 import cors from "cors";
 import "./db/database";
 import "./db/seed";
+import clientsRoutes from "./routes/clients";
+import invoicesRoutes from "./routes/invoices";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/clients", clientsRoutes);
+app.use("/api/invoices", invoicesRoutes);
 
 const PORT = 8000;
 
